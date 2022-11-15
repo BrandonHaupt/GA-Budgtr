e36e4f0bffdc8cdf05febba8c9e7ef9c127d2659
Brandon Haupt <haupt.brandon@gmail.com>
2022-11-14 22:14:31 -0600

    Create multiple routes
    - created index.ejs
    - created show.ejs
    - created new.ejs
    - updated server.js to add in new routes called index, show, and new

--- server.js
+++ server.js
@@ -1,17 +1,46 @@
 require("dotenv").config()
 const express = require("express")
+const budgets = require("./models/budget")
 const app = express()
 const PORT = process.env.PORT
+const methodOverride = require('method-override')
 
+
+// MIDDLEWARE
+
+// Allows for the use of css in the public folder
+app.use('/static', express.static("public"))
+
+
+// // Allows us to use the delete Method
+// app.use(methodOverride('_method'))
 
 // HOME Route
-app.get('/', (req, res) => res.redirect('/budgets'))
+app.get('/', (req, res) => res.redirect('/budgets/'))
 
 // INDEX Route
 app.get('/budgets/', (req, res) => {
-    res.render('index.ejs')
+    res.render(
+        'index.ejs',
+        {
+            allBudgets:budgets
+        }
+    )
 })
 
+// SHOW Route
+app.get('/budgets/:index', (req,res) => {
+    res.render('show.ejs', {
+        budget: budgets[req.params.index],
+        index: req.params.index
+    })
+})
+
+
+// NEW Route
+app.get(`/budgets/new`, (req,res) => {
+    res.render('new.ejs')
+})
 
 
 // Port listening
